2930745dfc713437550381eb971582a1a3b35597
Cody Mullin <CodyMullin@hey.com>
2021-03-11 23:31:43 -0500

    First Commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,64 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div>
+      Login
+    </div>
+    <form @submit.prevent="signin()">
+      <div>
+        <label for="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          v-model="username"
+          placeholder="Enter Username"
+        />
+      </div>
+      <div>
+        <label for="psername">Password</label>
+        <input
+          type="text"
+          id="password"
+          v-model="password"
+          placeholder="Enter Password"
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+    {{ $store.state.accessToken }}
+    <button @click="signout()">Log Out</button>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+import { server } from "../helper";
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    signin() {
+      let loginInfo = {
+        username: this.username,
+        password: this.password,
+      };
+      this.__submitToServer(loginInfo);
+    },
+    __submitToServer(data) {
+      axios
+        .post(`${server.baseURL}auth/signin`, data)
+        .then((data) => this.$store.commit("setToken", data.data.accessToken))
+        .then(() =>
+          localStorage.setItem("accessToken", this.$store.state.accessToken)
+        );
+    },
+    signout() {
+      this.$store.commit("removeToken", "");
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,116 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      Hello World
+    </div>
+    <div>{{ materials }}</div>
+
+    <form
+      v-on:submit.prevent="
+        createMaterial(
+          name,
+          description,
+          salesCost,
+          purchaseCost,
+          installCost,
+          category,
+          measurement
+        )
+      "
+    >
+      <div>
+        <label for="Name">Name</label>
+        <input type="text" class="border-2" v-model="newMaterial.name" />
+      </div>
+      <div>
+        <label for="Name">Description</label>
+        <input type="text" class="border-2" v-model="newMaterial.description" />
+      </div>
+      <div>
+        <label for="Name">Sales Cost</label>
+        <input type="text" class="border-2" v-model="newMaterial.salesCost" />
+      </div>
+      <div>
+        <label for="Name">Purchase Cost</label>
+        <input
+          type="text"
+          class="border-2"
+          v-model="newMaterial.purchaseCost"
+        />
+      </div>
+      <div>
+        <label for="Name">Install Cost</label>
+        <input type="text" class="border-2" v-model="newMaterial.installCost" />
+      </div>
+      <div>
+        <label for="Name">Category</label>
+        <input type="text" class="border-2" v-model="newMaterial.category" />
+      </div>
+      <div>
+        <label for="Name">Measurement</label>
+        <input type="text" class="border-2" v-model="newMaterial.measurement" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      materials: [],
+      newMaterial: {
+        name: "",
+        description: "",
+        salesCost: "",
+        purchaseCost: "",
+        installCost: "",
+        category: "",
+        measurement: "",
+      },
+      accessToken: this.$store.state.accessToken,
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/materials", {
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`,
+        },
+      })
+      .then((response) => (this.materials = response.data));
+  },
+  methods: {
+    async createMaterial(
+      name,
+      description,
+      salesCost,
+      purchaseCost,
+      installCost,
+      category,
+      measurement
+    ) {
+      const response = await axios.post(
+        "http://localhost:3000/materials",
+        {
+          name,
+          description,
+          salesCost,
+          purchaseCost,
+          installCost,
+          category,
+          measurement,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${this.accessToken}`,
+          },
+        }
+      );
+      console.log(response);
+    },
+  },
+};
 </script>
